4c371829a2aee20d55913a2f12db25c32d0e7f77
Khalimudin <khalimudin@gmail.com>
2020-06-04 01:26:58 +0700

    semantic ui and vee-validate

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from "vue";
-import App from "./App.vue";
+import VueRouter from "vue-router";
+import VeeValidate from "vue-validate";
 import axios from "axios";
+
+import App from "./App.vue";
+
+Vue.use(VueRouter);
+Vue.use(VueValidate);
+
+const router = new VueRouter({
+  base: "/",
+  mode: "history",
+  routes: [{ path: "/path", component: component }],
+});
 
 Vue.config.productionTip = false;
 window.axios = axios;
@@ -8,4 +20,5 @@ axios.defaults.baseURL = "http://127.0.0.1:3333";
 
 new Vue({
   render: (h) => h(App),
+  router,
 }).$mount("#app");
