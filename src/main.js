3e62647feb9a3df7e7b84664b0eea202f2d6bb32
Khalimudin <khalimudin@gmail.com>
2020-06-04 01:18:18 +0700

    setup axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+window.axios = axios;
+axios.defaults.baseURL = "http://127.0.0.1:3333";
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
