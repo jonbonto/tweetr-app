a4d21ef7724f2c434acd7b74c14591abadd35eb0
Khalimudin <khalimudin@gmail.com>
2020-06-04 04:44:28 +0700

    reply tweet

--- src/components/Tweet/SingleTweet.vue
+++ src/components/Tweet/SingleTweet.vue
@@ -24,18 +24,48 @@
           </div>
         </div>
       </div>
+
       <div class="ui divider"></div>
+
+      <form class="ui form" @submit.prevent="replyTweet">
+        <div class="field">
+          <textarea
+            name="reply"
+            v-model="reply"
+            rows="2"
+            placeholder="Reply tweet"
+          ></textarea>
+        </div>
+
+        <button class="ui button primary" :disabled="!isFormValid">
+          Reply
+        </button>
+      </form>
+
+      <Replies :replies="replies" />
     </div>
   </div>
 </template>
 
 <script>
+import Replies from "@/components/Tweet/Replies";
+
 export default {
   name: "SingleTweet",
+  components: {
+    Replies,
+  },
   data() {
     return {
       tweet: "",
+      replies: [],
+      reply: "",
     };
+  },
+  computed: {
+    isFormValid() {
+      return !!this.reply;
+    },
   },
   created() {
     this.fetchTweet();
@@ -48,6 +78,25 @@ export default {
     },
     back() {
       this.$router.go(-1);
+    },
+    replyTweet() {
+      const token = localStorage.getItem("tweetr-token");
+
+      axios
+        .post(
+          `/tweets/reply/${this.tweet.id}`,
+          { reply: this.reply },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((response) => {
+          this.reply = "";
+
+          this.replies.unshift(response.data.data);
+        });
     },
   },
 };
